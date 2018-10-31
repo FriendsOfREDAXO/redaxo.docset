c9c05cab954b560f5c3dba9aec604e34215cd712
Hartmut Junker <mail@hartmutjunker.de>
2018-10-31 14:00:07 +0100

    Switch to pipenv, reformatted source code

--- generate.py
+++ generate.py
@@ -9,58 +9,63 @@ api_url = "http://www.redaxo.org/api/master/"
 
 http = urllib3.PoolManager()
 
-response = http.request('GET', api_url)
+response = http.request("GET", api_url)
 soup = bs(response.data, "html.parser")
 
 # Datenbank einrichten
 
-db_file = 'redaxo.docset/Contents/Resources/docSet.dsidx'
+db_file = "redaxo.docset/Contents/Resources/docSet.dsidx"
 os.remove(db_file)
 
 conn = sqlite3.connect(db_file)
 cur = conn.cursor()
 
-try: cur.execute('DROP TABLE searchIndex;')
-except: pass
-cur.execute('CREATE TABLE searchIndex(id INTEGER PRIMARY KEY, name TEXT, type TEXT, path TEXT);')
-cur.execute('CREATE UNIQUE INDEX anchor ON searchIndex (name, type, path);')
+try:
+    cur.execute("DROP TABLE searchIndex;")
+except:
+    pass
+cur.execute(
+    "CREATE TABLE searchIndex(id INTEGER PRIMARY KEY, name TEXT, type TEXT, path TEXT);"
+)
+cur.execute("CREATE UNIQUE INDEX anchor ON searchIndex (name, type, path);")
 
 
 # API-Daten in DB eintragen
 
 categories = soup.find(id="elements").find_all("h3")
 entry_types = {
-    'Classes': 'Class',
-    'Interfaces': 'Interface',
-    'Traits': 'Trait',
-    'Exceptions': 'Exception',
-    'Functions': 'Function'
+    "Classes": "Class",
+    "Interfaces": "Interface",
+    "Traits": "Trait",
+    "Exceptions": "Exception",
+    "Functions": "Function",
 }
-chapters = {
-    'methods': 'Method',
-    'constants': 'Constant',
-    'properties': 'Property'
-}
+chapters = {"methods": "Method", "constants": "Constant", "properties": "Property"}
 api_docs = []
 
 c1 = categories[2]
-c2 = c1.next_sibling.next_sibling.find_all('a')
+c2 = c1.next_sibling.next_sibling.find_all("a")
 
 for cat in categories:
-    for link in cat.next_sibling.next_sibling.find_all('a'):
-        if link.contents[0].name == 'span':
+    for link in cat.next_sibling.next_sibling.find_all("a"):
+        if link.contents[0].name == "span":
             name = link.contents[0].contents[0]
         else:
             name = link.contents[0]
         if not re.match(r"apigen", name, re.IGNORECASE):
-            cur.execute('INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (?,?,?)', (name, cat.contents[0], link['href']))
+            cur.execute(
+                "INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (?,?,?)",
+                (name, cat.contents[0], link["href"]),
+            )
             conn.commit()
-            api_docs.append(link['href'])
+            api_docs.append(link["href"])
 
 conn.close()
 
 categories_list = [cat.contents[0] for cat in categories]
-print(f"Generated database index with {', '.join(categories_list[:-1])} and {categories_list[-1]}")
+print(
+    f"Generated database index with {', '.join(categories_list[:-1])} and {categories_list[-1]}"
+)
 
 
 # Templating
@@ -82,7 +87,7 @@ template = """
 htmlDestPath = "redaxo.docset/Contents/Resources/Documents"
 imagesPath = "redaxo.docset/Contents/Resources/Assets/images"
 
-for f in glob.glob('redaxo.docset/Contents/Resources/Documents/*'):
+for f in glob.glob("redaxo.docset/Contents/Resources/Documents/*"):
     os.remove(f)
 
 # HTML laden und manipulieren
@@ -93,16 +98,16 @@ docs_processed = 0
 for page in api_docs:
 
     # Doku laden
-    response = http.request('GET', api_url + page)
+    response = http.request("GET", api_url + page)
     doc_soup = bs(response.data, "html.parser")
 
     # find images
-    for i in [img['src'] for img in doc_soup.find_all('img')]:
+    for i in [img["src"] for img in doc_soup.find_all("img")]:
         images.add(i)
 
-    # ... und URLs anpassen
-    for i in doc_soup.find_all('img') :
-        i['src'] = '../Assets/images/' + os.path.basename(i['src'])
+    # ... and modify URLs
+    for i in doc_soup.find_all("img"):
+        i["src"] = "../Assets/images/" + os.path.basename(i["src"])
     page_content = doc_soup.find(id="content")
 
     for id in chapters:
@@ -110,9 +115,20 @@ for page in api_docs:
         if docs:
             links_to_sources = docs.find_all("a", title="Go to source code")
             for anchor in links_to_sources:
-                anchor['href'] = re.sub(r"^(.*\.html)#(\d+)-(\d+)", r"\1?\2,\3#\2", anchor['href'], 0, re.MULTILINE)
+                anchor["href"] = re.sub(
+                    r"^(.*\.html)#(\d+)-(\d+)",
+                    r"\1?\2,\3#\2",
+                    anchor["href"],
+                    0,
+                    re.MULTILINE,
+                )
                 toc = soup.new_tag("a")
-                toc["name"] = f"//apple_ref/cpp/{chapters[id]}/" + urllib.parse.quote_plus(str([child for child in anchor.descendants][-1]))
+                toc["name"] = (
+                    f"//apple_ref/cpp/{chapters[id]}/"
+                    + urllib.parse.quote_plus(
+                        str([child for child in anchor.descendants][-1])
+                    )
+                )
                 toc["class"] = "dashAnchor"
                 anchor.insert_before(toc)
 
@@ -124,24 +140,27 @@ for page in api_docs:
         file.write(htmlOut)
 
     # Source laden
-    response = http.request('GET', api_url + 'source-' + page)
+    response = http.request("GET", api_url + "source-" + page)
     source_soup = bs(response.data, "html.parser")
     page_content = source_soup.find(id="source")
+
+    # for a in page_content.find_all("a"):
+    #    del a['href']
 
     # modifizierten Source speichern
     taret_source_soup = bs(template % page_content, "html.parser")
     htmlOut = taret_source_soup.prettify("utf-8")
-    with open("%s/%s" % (htmlDestPath, 'source-' + page), "wb") as file:
+    with open("%s/%s" % (htmlDestPath, "source-" + page), "wb") as file:
         file.write(htmlOut)
 
     docs_processed += 2
 
-    if (docs_processed % 50 == 0):
-        print(f'{docs_processed} of {num_docs_to_process} documents processed')
+    if docs_processed % 50 == 0:
+        print(f"{docs_processed} of {num_docs_to_process} documents processed")
 
 # Bilder speichern
 
 for i in images:
-    response = http.request('GET', api_url + i)
+    response = http.request("GET", api_url + i)
     with open("%s/%s" % (imagesPath, os.path.basename(i)), "wb") as file:
         file.write(response.data)
